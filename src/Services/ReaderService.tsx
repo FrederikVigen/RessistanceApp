90cb5927a9056c033a557fd3c3541044920f0ca3
FrederikVigen <frederikvigen@gmail.com>
2022-02-19 13:03:39 +0100

    Finished sliders for rate and linebreak

--- src/Services/ReaderService.tsx
+++ src/Services/ReaderService.tsx
@@ -1,17 +1,37 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 export interface IReaderService {
-    readText(str: string): void
+    readText(strs: string[]): void
+    setRate(rate: number): void
+    rate: number
+    setLineBreak(lineBreak: number): void
+    lineBreak: number
+    defaultLineBreak: number
+    defaultRate: number
 }
 
 export const ReaderserviceContext = createContext<IReaderService | undefined>(undefined);
 
 const ReaderService = ({children}: any) => {
-    
+    const synth = window.speechSynthesis
+    const defaultRate = 1
+    const defaultLineBreak = 1
+    const [rate, setRate] = useState(defaultRate)
+    const [lineBreak, setLineBreak] = useState(defaultLineBreak)
+
     const readerService : IReaderService = {
-        readText: (str) => {
-            console.log(str)
-        }
+        readText: (strs) => {
+            for(var str in strs) {
+                var utterance = new SpeechSynthesisUtterance(str)
+                synth.speak(utterance)
+            }
+        },
+        rate: rate,
+        setRate: setRate,
+        lineBreak: lineBreak,
+        setLineBreak: setLineBreak,
+        defaultRate: defaultRate,
+        defaultLineBreak: defaultLineBreak
     }
   
     return (
--- src/components/ReaderControls.tsx
+++ src/components/ReaderControls.tsx
@@ -1,13 +1,31 @@
-import { Button, Stack } from '@mui/material'
+import { Box, Button, FormControlLabel, Slider, Stack, Typography } from '@mui/material'
 import React from 'react'
 import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded';
+import { useReaderService } from '../Services/ReaderService';
+import { useTextService } from '../Services/TextService';
 
 const ReaderControls = () => {
+    const readerService = useReaderService()
+    const textSerivice = useTextService()
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        readerService.readText([textSerivice.currentText[0]])
+    }
+
+    const handleRateChange = (e : Event, value: number | number[], activeThumb: number) => readerService.setRate(value as number) 
+    const handleLineBreak = (e : Event, value: number | number[], activeThumb: number) => readerService.setLineBreak(value as number) 
+
     return (
-        <Stack spacing={2} direction="row" justifyContent={"center"}>
-            <Button variant="contained">
+        <Stack spacing={2} direction="column" justifyContent={"center"}>
+            <Button variant="contained" onClick={handleClick}>
                 <PlayArrowRoundedIcon />
             </Button>
+            <Stack flex={1}>
+                <Typography>Rate: {readerService.rate}</Typography>   
+                <Slider onChange={handleRateChange} step={.1} marks min={.5} max={2} defaultValue={readerService.defaultRate} valueLabelDisplay="auto" />
+                <Typography>Break between lines in seconds: {readerService.lineBreak}</Typography>   
+                <Slider onChange={handleLineBreak} step={.5} marks min={1} max={5} defaultValue={readerService.defaultLineBreak} valueLabelDisplay="auto" />
+            </Stack>
         </Stack>
     )
 }
